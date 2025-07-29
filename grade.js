86de58b765b46e2ba193048cef9ceed3310ce126
Rabbi Khan <Krabby2244@gmail.com>
2025-07-29 23:51:01 +0600

    added grade.js

---
+++ grade.js
@@ -0,0 +1,15 @@
+let marks = 61;
+ if(marks>=90 && marks<=100){
+    console.log("A+");
+ }else if(marks>=80 && marks<=89){
+    console.log("A");
+ }else if(marks>=70 && marks<=79){
+    console.log("A-");
+ }else if(marks>=60 && marks<69){
+    console.log("B");
+ }else if(marks>=0 && marks<=59){
+    console.log("F");
+ }else{
+    console.log("Invalid Score");
+ }
+ 
